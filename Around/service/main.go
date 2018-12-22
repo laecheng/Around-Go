2e0311fabf196f639215842676574edf4fd4520f
laecheng <chc286@ucsd.edu>
2018-12-22 15:39:45 -0800

    go handle post, search

--- Around/service/main.go
+++ Around/service/main.go
@@ -17,12 +17,11 @@ import (
 )
 
 const (
-	POST_INDEX = "post"
-	POST_TYPE  = "post"
-
+	POST_INDEX  = "post"
+	POST_TYPE   = "post"
 	DISTANCE    = "200km"
-	ES_URL      = "http://35.196.1.62:9200"
-	BUCKET_NAME = "kevin-post-image"
+	ES_URL      = "http://35.229.30.107:9200/"
+	BUCKET_NAME = "post-images-kevin"
 )
 
 type Location struct {
@@ -46,107 +45,34 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handlerPost(w http.ResponseWriter, r *http.Request) {
-	// Parse from body of request to get a json object.
-	fmt.Println("Received one post request")
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
-
-	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
-	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
-
-	fmt.Printf("Parsed lat from client: %v\n", lat)
-	fmt.Printf("Parsed lat from client: %v\n", lon)
-
-	p := &Post{
-		User:    r.FormValue("user"),
-		Message: r.FormValue("message"),
-		Location: Location{
-			Lat: lat,
-			Lon: lon,
-		},
-	}
-
-	id := uuid.New()
-	file, _, err := r.FormFile("image")
-	if err != nil {
-		http.Error(w, "Image is not available", http.StatusBadRequest)
-		fmt.Printf("Image is not available %v.\n", err)
-		return
-	}
-	attrs, err := saveToGCS(file, BUCKET_NAME, id)
-	if err != nil {
-		http.Error(w, "Failed to save image to GCS", http.StatusInternalServerError)
-		fmt.Printf("Failed to save image to GCS %v.\n", err)
-		return
-	}
-	p.Url = attrs.MediaLink
-
-	err = saveToES(p, id)
-	if err != nil {
-		http.Error(w, "Failed to save post to ElasticSearch", http.StatusInternalServerError)
-		fmt.Printf("Failed to save post to ElasticSearch %v.\n", err)
-		return
-	}
-	fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
-}
-
-func handlerSearch(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one request for search")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
-
-	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
-	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
-	// range is optional
-	ran := DISTANCE
-	if val := r.URL.Query().Get("range"); val != "" {
-		ran = val + "km"
-	}
-
-	posts, err := readFromES(lat, lon, ran)
-	if err != nil {
-		http.Error(w, "Failed to read post from ElasticSearch", http.StatusInternalServerError)
-		fmt.Printf("Failed to read post from ElasticSearch %v.\n", err)
-		return
-	}
-
-	js, err := json.Marshal(posts)
-	if err != nil {
-		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
-		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
-		return
-	}
-
-	w.Write(js)
-}
-
 func createIndexIfNotExist() {
+	// the checking abnormal feature of Elastic Search not available for external IP
+	// so we need to set it to false
 	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
 	if err != nil {
 		panic(err)
 	}
-
+	// check POST_INDEX index already exists in Elastic Search
 	exists, err := client.IndexExists(POST_INDEX).Do(context.Background())
 	if err != nil {
 		panic(err)
 	}
-
+	// if index not exists
 	if !exists {
 		mapping := `{
-            "mappings": {
-                "post": {
-                    "properties": {
-                        "location": {
-                            "type": "geo_point"
-                        }
-                    }
-                }
-            }
-        }`
+			"mappings": {
+				"post": {
+					"properties": {
+						"location": {
+							"type": "geo_point"
+						}
+					}
+				}
+			}
+		}`
+		// context.Background is for Go Routine
+		// generally when have a new Go Routine, we need add this
+		// you can add global variables to context
 		_, err = client.CreateIndex(POST_INDEX).Body(mapping).Do(context.Background())
 		if err != nil {
 			panic(err)
@@ -154,13 +80,15 @@ func createIndexIfNotExist() {
 	}
 }
 
-// Save a post to ElasticSearch
+// helper function for handlerPost
 func saveToES(post *Post, id string) error {
+	// Create the connection to Elastic Search
 	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
 	if err != nil {
-		return err
+		panic(err)
 	}
 
+	// Insert the post to Index=POST_INDEX, Type=POST_TYPE
 	_, err = client.Index().
 		Index(POST_INDEX).
 		Type(POST_TYPE).
@@ -171,20 +99,62 @@ func saveToES(post *Post, id string) error {
 	if err != nil {
 		return err
 	}
-
-	fmt.Printf("Post is saved to index: %s\n", post.Message)
+	fmt.Printf("Post is saved to index: %s", post.Message)
 	return nil
 }
 
-func readFromES(lat, lon float64, ran string) ([]Post, error) {
-	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+// helper function for handlerPost
+func saveToGCS(r io.Reader, bucketName, objectName string) (*storage.ObjectAttrs, error) {
+	// for Go Routine
+	ctx := context.Background()
+
+	// create a connection with GCS
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile("E:\\keys\\Around-65b31ceb88d1.json"))
 	if err != nil {
 		return nil, err
 	}
 
-	query := elastic.NewGeoDistanceQuery("location")
-	query = query.Distance(ran).Lat(lat).Lon(lon)
+	// check if bucket exist
+	bucket := client.Bucket(bucketName)
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, err
+	}
 
+	// create a object to store into GCS
+	object := bucket.Object(objectName)
+	wc := object.NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, err
+	}
+
+	// set storage avaiable to all users (public)
+	if err = object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, err
+	}
+
+	// return the object's attr
+	attrs, err := object.Attrs(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink)
+	return attrs, nil
+}
+
+// helper function for handlerSearch
+func readFromES(lat, lon float64, ran string) ([]Post, error) {
+	// Create the connection to Elastic Search
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		panic(err)
+	}
+
+	// Search from Elastic Search
+	query := elastic.NewGeoDistanceQuery("location").Distance(ran).Lat(lat).Lon(lon)
 	searchResult, err := client.Search().
 		Index(POST_INDEX).
 		Query(query).
@@ -196,7 +166,7 @@ func readFromES(lat, lon float64, ran string) ([]Post, error) {
 
 	// searchResult is of type SearchResult and returns hits, suggestions,
 	// and all kinds of other information from Elasticsearch.
-	fmt.Printf("Query took %d milliseconds\n", searchResult.TookInMillis)
+	fmt.Printf("Elastic Search took %d milliseconds\n", searchResult.TookInMillis)
 
 	// Each is a convenience function that iterates over hits in a search result.
 	// It makes sure you don't need to check for nil values in the response.
@@ -213,38 +183,86 @@ func readFromES(lat, lon float64, ran string) ([]Post, error) {
 	return posts, nil
 }
 
-func saveToGCS(r io.Reader, bucketName, objectName string) (*storage.ObjectAttrs, error) {
-	ctx := context.Background()
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
 
-	// Creates a client.
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile("E:\\keys\\Around-4d648e2f3976.json"))
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	// parse the lat and lon from request
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
+
+	// construct a post
+	p := Post{
+		User:    r.FormValue("user"),
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+
+	// save binary file to GCS
+	id := uuid.New()
+	file, _, err := r.FormFile("image")
 	if err != nil {
-		return nil, err
+		http.Error(w, "Image is not available", http.StatusBadRequest)
+		fmt.Printf("Image is not available %v.\n", err)
+		return
 	}
-
-	bucket := client.Bucket(bucketName)
-	if _, err := bucket.Attrs(ctx); err != nil {
-		return nil, err
-	}
-
-	object := bucket.Object(objectName)
-	wc := object.NewWriter(ctx)
-	if _, err = io.Copy(wc, r); err != nil {
-		return nil, err
-	}
-	if err := wc.Close(); err != nil {
-		return nil, err
-	}
-
-	if err = object.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
-		return nil, err
-	}
-
-	attrs, err := object.Attrs(ctx)
+	attrs, err := saveToGCS(file, BUCKET_NAME, id)
 	if err != nil {
-		return nil, err
+		http.Error(w, "Failed to save image to GCS", http.StatusInternalServerError)
+		fmt.Printf("Failed to save image to GCS %v.\n", err)
+		return
+	}
+	p.Url = attrs.MediaLink
+
+	// Send the Post to Elastic Search
+	err = saveToES(&p, id)
+	if err != nil {
+		http.Error(w, "Failed to save post to ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save post to ElasticSearch %v.\n", err)
+		return
+	}
+	fmt.Printf("Saved one post to ElasticSearch: %s", p.Message)
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one search request")
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+
+	// parse the lat and lon from request
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+
+	// Elastic Search require distance as string
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"
 	}
 
-	fmt.Printf("Image is saved to GCS: %s\n", attrs.MediaLink)
-	return attrs, nil
+	// search from Elastic Search
+	posts, err := readFromES(lat, lon, ran)
+	if err != nil {
+		http.Error(w, "Failed to read post from ElasticSearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to read post from ElasticSearch %v.\n", err)
+		return
+	}
+
+	// serilize the post to JSON and write to response
+	js, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, "Failed to parse posts into JSON format", http.StatusInternalServerError)
+		fmt.Printf("Failed to parse posts into JSON format %v.\n", err)
+		return
+	}
+	w.Write(js)
 }
